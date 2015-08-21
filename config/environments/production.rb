6490574302bd39f264019fe2c191c2ba3f7ab72b
worldinmyeyes <wojciechszczerek@wp.pl>
2015-08-21 16:12:34 +0100

    corrected username and password config for sendgrid #1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,12 +72,11 @@ Rails.application.configure do
   address:              'smtp.sendgrid.net',
   port:                 587,
   domain:               'heroku.com',
-  user_name:            ENV['app40336263@heroku.com'],
-  password:             ENV['qajtyvz46016'],
+  user_name:            ENV['SENDGRID_USERNAME'],
+  password:             ENV['SENDGRID_PASSWORD'],
   authentication:       'plain',
   enable_starttls_auto: true  }
-
-
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
