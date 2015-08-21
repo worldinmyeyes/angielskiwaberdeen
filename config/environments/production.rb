a8c3ab69154cc19a45953a0cd462b1f769b02a74
worldinmyeyes <wojciechszczerek@wp.pl>
2015-08-21 15:50:20 +0100

    updated pword in mailer details in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   port:                 587,
   domain:               'gmail.com',
   user_name:            'wojciechszczerek@gmail.com',
-  password:             'mypassword',
+  password:             'noweHory15',
   authentication:       'plain',
   enable_starttls_auto: true  }
 
